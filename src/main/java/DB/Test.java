a8505a08e3095604dda74f32b24b34b2979d2668
JT-611 <jyotsnat611@gmail.com>
2023-02-28 12:55:07 +0530

    fifth commit

--- src/main/java/DB/Test.java
+++ src/main/java/DB/Test.java
@@ -2,10 +2,9 @@ package DB;
 
 public class Test 
 {
-	public static void main(Sting... g)
+	public static void main(String... g)
 	{
-		
+		System.out.println("Hello1 Test 1");
 		System.out.println("Hello ! STS project upload on git");
-		
 	}
 }
---
+++ src/main/java/DB/Test2.java
@@ -0,0 +1,9 @@
+package DB;
+
+public class Test2 {
+	public static void main(String[] args)
+	{
+		System.out.println("Hello2 ! STS uploading on git");
+		System.out.println("Hello2 test 2");
+	}
+}
